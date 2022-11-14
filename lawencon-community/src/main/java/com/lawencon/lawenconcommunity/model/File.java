cf8408a3c69a096e932ba80196af1dc8a939f016
rifqidzak <rifdzak@gmail.com>
2022-11-14 16:36:02 +0700

    Added Model By Muhammad Rifqi Dzaky Siara

---
+++ lawencon-community/src/main/java/com/lawencon/lawenconcommunity/model/File.java
@@ -0,0 +1,26 @@
+package com.lawencon.lawenconcommunity.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.lawencon.base.BaseEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name = "tb_file")
+@Entity
+@Getter
+@Setter
+public class File extends BaseEntity {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2193244827181488175L;
+	@Column(name = "file")
+	private String file;
+	
+	@Column(name = "ext", length = 5)
+	private String ext;
+}
