a6ac0bdafe63d6bd49ca90f4123660e87e5d7788
rifqidzak <rifdzak@gmail.com>
2022-11-14 17:06:32 +0700

    Added Application Service, Added UserService, Add Dependencies , Add SecurityConfig by Muhammad Rifqi Dzaky Siara

---
+++ lawencon-community/src/main/java/com/lawencon/lawenconcommunity/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.lawencon.lawenconcommunity.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import io.swagger.v3.oas.models.PathItem.HttpMethod;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean(name = "webIgnoring")
+	public List<RequestMatcher> Matchers() {
+
+		final List<RequestMatcher> requestMatchers = new ArrayList<>();
+		requestMatchers.add(new AntPathRequestMatcher("/files/**", HttpMethod.GET.name()));
+		requestMatchers.add(new AntPathRequestMatcher("/swagger-ui/**", HttpMethod.GET.name()));
+		requestMatchers.add(new AntPathRequestMatcher("/v3/**", HttpMethod.GET.name()));
+		requestMatchers.add(new AntPathRequestMatcher("/login/**", HttpMethod.POST.name()));
+		return requestMatchers;
+	}
+}
