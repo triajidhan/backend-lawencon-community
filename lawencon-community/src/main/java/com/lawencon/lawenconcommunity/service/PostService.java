351d7f18a9082f2a781f90f332f0f37d44de2d5c
triajidhan <triajidhan@gmail.com>
2022-12-08 09:18:40 +0700

    edit post service

--- lawencon-community/src/main/java/com/lawencon/lawenconcommunity/service/PostService.java
+++ lawencon-community/src/main/java/com/lawencon/lawenconcommunity/service/PostService.java
@@ -249,7 +249,7 @@ public class PostService extends BaseCoreService {
 		List<String>commentId =  new ArrayList<>();
 		List<String>commentBody =  new ArrayList<>();
 		List<LocalDateTime>createdAtComment=  new ArrayList<>();
-		List<Comment>comments = commentDao.getByPostAndOrder(post.getId(), 0, 2, false);
+		List<Comment>comments = commentDao.getByPostAndOrder(post.getId(),true);
 		for(int l = comments.size()-1 ; l>=0; l--) {
 			userComment.add(comments.get(l).getUser());
 			commentBody.add(comments.get(l).getCommentBody());
